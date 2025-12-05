39923c207ecee3732bc7824eeb8aacf044dc5cea
onethelilypad <ihbmohdjuhari1@sheffield.ac.uk>
2025-12-05 20:05:32 +0000

    Add data transformation steps

--- data_preparation.py
+++ data_preparation.py
@@ -8,3 +8,11 @@ df.columns = [col.strip().lower().replace(' ', '-') for col in df.columns]
 
 print("\nData after Cleaning:")
 print(df.head())
+
+from sklearn.preprocessing import LabelEncoder
+
+le = LabelEncoder()
+df['species'] = le.fit_transform(df['species'])
+
+print("\nData after transformation")
+print(df.head())
